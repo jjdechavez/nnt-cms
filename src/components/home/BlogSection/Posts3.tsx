1593a455a080acef9d0cac01dbbd651aef8b093e
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2022-05-07 12:04:35 +0800

    Fixed unknown property on BlogSection Post3

--- src/components/home/BlogSection/Posts3.tsx
+++ src/components/home/BlogSection/Posts3.tsx
@@ -26,10 +26,10 @@ const Posts3: React.FC<{
                     className="w-4 h-4 ml-2"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    stroke-width="2"
+                    strokeWidth="2"
                     fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   >
                     <path d="M5 12h14"></path>
                     <path d="M12 5l7 7-7 7"></path>
